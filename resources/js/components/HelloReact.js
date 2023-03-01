08fb65c53ec4564a2e3e6cad4111ed1cba1bb5cb
Stanley Dsouza <lancylotdsouza@gmail.com>
2023-03-01 16:52:38 +0530

    react template integrated

--- resources/js/components/HelloReact.js
+++ resources/js/components/HelloReact.js
@@ -2,10 +2,35 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { PDFViewer } from '@react-pdf/renderer';
+import MyDocument from './Pdf.js';
 
 export default function HelloReact() {
     return (
-        <h1>Hello React!</h1>
+                <div>
+                    <h3>Form</h3>
+                    <form>
+                        <div className="row">
+                            <div className="col-md-6">
+                                <div className="input-group input-group-outline my-3">
+                                    <label className="form-label">Email</label>
+                                    <input type="email" className="form-control" />
+                                </div>
+                            </div>
+                            <div className="col-md-6">
+                                <div className="input-group input-group-outline my-3">
+                                    <label className="form-label">Email</label>
+                                    <input type="email" className="form-control" disabled />
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </form>
+                    <h3>PDF</h3>
+                    <PDFViewer>
+                        <MyDocument />
+                    </PDFViewer>
+                </div>
     );
 }
 
